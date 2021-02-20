ccc3f6adc2f494ef0446f06e7421eecab7c93300
이지은 <wldms8196@gmail.com>
2021-02-20 15:47:45 +0900

    modity

--- src/components/Insert.js
+++ src/components/Insert.js
@@ -16,8 +16,6 @@ const Insert = ({onInsert}) => {
         setValue('')
     }
 
-    
-
     return(
         <form className="insertForm" onSubmit={onSubmit}>
             <input className="insertInput" autoFocus placeholder="오늘의 할 일을 추가하세요." value={value} onChange={onChange} required/>
